2f5707dd0b855d57f3f14161a8a78008e6b82242
YiLiu Wang <1559148381@qq.com>
2018-05-05 21:37:40 +0800

    nth_element
    The method of the nth_element

---
+++ SortAccordingThe_nth/demo/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <algorithm>
+#include <deque>
+#include <functional>
+#include <iterator>
+
+using namespace std;
+
+int main()
+{
+	deque<int> ideq;
+
+	for (int i = 3; i <= 7; ++i)
+		ideq.push_back(i);
+	for (int i = 2; i <= 6; ++i)
+		ideq.push_back(i);
+	for (int i = 1; i <= 5; ++i)
+		ideq.push_back(i);
+
+	/*for (deque<int>::iterator iter = ideq.begin();
+		iter != ideq.end(); ++iter)
+		cout << *iter << ' ';
+	cout << endl;
+
+	nth_element(ideq.begin(), ideq.begin() + 3, ideq.end());
+	for (deque<int>::iterator iter = ideq.begin();
+		iter != ideq.end(); ++iter)
+		cout << *iter << ' ';
+	cout << endl;
+
+	copy(ideq.begin(), ideq.begin() + 4, ostream_iterator<int>(cout, " "));
+	cout << endl;*/
+
+	nth_element(ideq.begin(), ideq.end() - 4, ideq.end());
+	for (deque<int>::iterator iter = ideq.begin();
+		iter != ideq.end(); ++iter)
+		cout << *iter << ' ';
+	cout << endl;
+	copy(ideq.end() - 4, ideq.end(), ostream_iterator<int>(cout, " "));
+	cout << endl;
+
+	nth_element(ideq.begin(), ideq.begin() + 3, ideq.end(), greater<int>());
+	copy(ideq.begin(), ideq.begin() + 4, ostream_iterator<int>(cout, " "));
+	cout << endl;
+
+	deque<int>::iterator pos;
+	pos = partition(ideq.begin(), ideq.end(), bind2nd(less_equal<int>(), 3));
+	copy(ideq.begin(), pos, ostream_iterator<int>(cout, " "));
+	cout << endl;
+
+	system("pause");
+	return 0;
+}
